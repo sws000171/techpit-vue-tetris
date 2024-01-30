3bfd6bd8ef5965eccc3252777d52fab467c1b6e6
sws000171 <higuchi@software-s.co.jp>
2024-01-30 17:50:47 +0900

    1/30

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,30 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//vueRouter で必要なモジュールを読み込む
+import { RouteRecordRaw,createWebHistory,createRouter } from 'vue-router';
+
+//トップページとプレイページを読み込む
+import TopPage from "./pages/TopPage.vue";
+import PlayPage from "./pages/PlayPage.vue";
+
+//各画面のルーティン情報を記述する
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "TopPage",
+    component: TopPage,
+  },
+  {
+    path: "/play",
+    name: "PlayPage",
+    component: PlayPage,
+  },
+];
+//各画面のルーティン情報からルーターを作成する
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  });
+
+createApp(App).use(router).mount('#app')
---
+++ src/pages/PlayPage.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+  const row = 20;
+  const column = 10;
+  const filed = new Array(row);
+
+  for (let i = 0 ; i < row ;i++){
+    const filedColumn = new Array(column).fill(0);
+    filed[i] = filedColumn;
+  }
+
+</script>
+
+<template>
+  <h1>プレイ画面</h1>
+  <h2>ユーザ名: {{ $route.query.name }}</h2>
+
+  <div class="container">
+    <table class="filed" style="border-collapse: collapse;">
+      <tr v-for="(row,y) in filed" :key="y" >
+        <td
+          class="block"
+          v-for="(col,x) in row" :key="() => `${x}${y}`" >
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<!-- テトリスのフィールドのデザインを整えるためのスタイルシート -->
+<!-- scoped 対象のコンポーネント内のみ適用-->
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+}
+.field {
+  border: ridge 0.4em #2c3e50;
+  border-top: none;
+} 
+.block {
+  width: 1em;
+  height: 1em;
+  border: 0.1px solid #95a5a6;
+}
+</style>
